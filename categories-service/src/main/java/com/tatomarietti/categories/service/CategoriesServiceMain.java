66829deb3ded65f74fb9d63f32193ebd66357580
Santiago Marietti <santiago.marietti@roche.com>
2020-05-22 10:16:47 -0300

    initial springboot app

---
+++ categories-service/src/main/java/com/tatomarietti/categories/service/CategoriesServiceMain.java
@@ -0,0 +1,13 @@
+package com.tatomarietti.categories.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesServiceMain {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CategoriesServiceMain.class, args);
+  }
+
+}
---
+++ categories-service/src/test/java/com/tatomarietti/categories/service/CategoriesServiceMainTest.java
@@ -0,0 +1,10 @@
+package com.tatomarietti.categories.service;
+
+import org.junit.jupiter.api.Test;
+
+class CategoriesServiceMainTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
